9a600c988a484666fd4f4e98b28bcd087ff35e0e
teddycihuyy <akashiroteddy@gmail.com>
2026-08-02 01:17:29 +0700

    Add message list and function to update messages

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
-
+const pesanList = [
+    "Kamu itu alasan aku senyum tiap hari",
+    "Frenkkkkkk🤪",
+    "Serius deh, bayar tapi",
+    "1 Agustus = hari buat ngingetin kalau aku sayang kamu banget",
+    "Makasih udah jadi tempat paling nyaman buat aku",
+    "btw ada 200k ga",
+    "pi boonk🤪🤪🤪"
+  ];
+  let i = 0;
+  function ubahPesan() {
+    document.getElementById('pesan').innerHTML = pesanList[i];
+    i = (i + 1) % pesanList.length;
+  }
